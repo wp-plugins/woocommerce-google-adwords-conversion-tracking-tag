2e6418cbf5b4e194b6d952f5778f50f009eed9cd
alekv <alekv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-01 20:08:32 +0000

    tagging version 0.2.2
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-google-adwords-conversion-tracking-tag/tags/0.2.2@1190939 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wgact.php
+++ wgact.php
@@ -1,14 +1,19 @@
 <?php
-/*********************************************************************************************************************************************************************************************************************************
-
+/**
 Plugin Name:  WooCommerce Google AdWords conversion tracking tag
-Plugin URI:   http://www.wolfundbaer.ch
-Description:  This plugin enables Google AdWords conversion value tracking for WooCommerce orders. This is important if you want to measure the ROI of your campaigns.
-Version:      0.1.8
-Author:       Wolf & Bär
+Plugin URI:   https://wordpress.org/plugins/woocommerce-google-adwords-conversion-tracking-tag/
+Description:  Google AdWords dynamic conversion value tracking for WooCommerce.
+Author:       Wolf & Bär GmbH
 Author URI:   http://www.wolfundbaer.ch
+Version:      0.2.2
+License:      GPLv2 or later
+Text Domain:  woocommerce-google-adwords-conversion-tracking-tag
+**/
 
-********************************************************************************************************************************************************************************************************************************/
+// Security measure: http://mikejolley.com/2013/08/keeping-your-shit-secure-whilst-developing-for-wordpress/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 
 
 class WGACT{
@@ -26,31 +31,56 @@ class WGACT{
 		}
 		*/
 		
+/**
+You might ask yourself why I am inserting the following function.
+A bit more information on that: Unfortunately there is a filter in WordPress (up to the current version 3.8.1) which messes up the Google AdWords tracking tag after injecting it into the thankyou page. The following function will fix this and is only a workaround until WordPress releases a fix. Accolaeds to awezlzel who posted this workaround. This bug was reported years ago and is still an open issue: http://core.trac.wordpress.org/ticket/3670
+**/
+		
+		function cdata_markupfix($content) { $content = str_replace("]]&gt;", "]]>", $content); return $content; }
+		function cdata_template_redirect( $content ) { ob_start('cdata_markupfix'); }
+		//add_action('template_redirect','cdata_template_redirect',-1);
+		
 		// add the Google AdWords tag to the footer of the page
-		add_action( 'wp_footer', array( $this, 'GoogleAdWordsTag' ));
+		//add_action( 'wp_head', array( $this, 'GoogleAdWordsTag' ));
+		
+		// add the Google AdWords tag to the thankyou part of the page within the body tags
+		add_action( 'woocommerce_thankyou', array( $this, 'GoogleAdWordsTag' ));
 		
 		// add the admin options page
-		add_action('admin_menu', array( $this, 'wgact_plugin_admin_add_page'));
+		// add_action('admin_menu', array( $this, 'wgact_plugin_admin_add_page'));
+		add_action('admin_menu', array( $this, 'wgact_plugin_admin_add_page'),99);
 		
 		// install a settings page in the admin console
 		add_action('admin_init', array( $this, 'wgact_plugin_admin_init'));
 		
 		// add a settings link on the plugins page
 		add_filter('plugin_action_links', array($this, 'wgact_settings_link'), 10, 2);
+		
+		// Load textdomain
+		// source: http://wordpress.stackexchange.com/questions/80334/how-to-make-a-wordpress-plugin-translation-ready
+		// source: http://fooplugins.com/make-a-wordpress-plugin-translations/
+		// source: http://geertdedeckere.be/article/loading-wordpress-language-files-the-right-way
+		add_action('init', array($this, 'load_plugin_textdomain'));
 
 		
+	}
+	
+	// Load text domain function
+	public function load_plugin_textdomain(){
+		load_plugin_textdomain('woocommerce-google-adwords-conversion-tracking-tag', FALSE, dirname(plugin_basename(__FILE__)).'/languages/');
 	}
 	
 	// adds a link on the plugins page for the wgact settings
 	function wgact_settings_link($links, $file) {
 		if ($file == plugin_basename(__FILE__))
-			$links[] = '<a href="' . admin_url("options-general.php?page=do_wgact") . '">'. __('Settings') .'</a>';
+			$links[] = '<a href="' . admin_url("admin.php?page=do_wgact") . '">'. __('Settings') .'</a>';
 		return $links;
 	}
 	
 	// add the admin options page
 	function wgact_plugin_admin_add_page() {
-		add_options_page('WGACT Plugin Page', 'WGACT Plugin Menu', 'manage_options', 'do_wgact', array($this, 'wgact_plugin_options_page'));
+		//add_options_page('WGACT Plugin Page', 'WGACT Plugin Menu', 'manage_options', 'do_wgact', array($this, 'wgact_plugin_options_page'));
+		add_submenu_page('woocommerce', 'AdWords Conversion Tracking', 'AdWords Conversion Tracking', 'manage_options', 'do_wgact', array($this, 'wgact_plugin_options_page'));
 	}
 
 	// display the admin options page
@@ -67,7 +97,7 @@ class WGACT{
 
 	<br>
 	<div style="background: #eee; width: 772px">
-		<div style="background: #ccc; padding: 10px; font-weight: bold">Configuration for the WooCommerce Google AdWords conversion tracking tag</div>
+		<div style="background: #ccc; padding: 10px; font-weight: bold"><?php _e( 'AdWords Conversion Tracking Settings', 'woocommerce-google-adwords-conversion-tracking-tag' ) ?></div>
 		<form action="options.php" method="post">
 		
 			<?php settings_fields('wgact_plugin_options'); ?>
@@ -88,13 +118,13 @@ class WGACT{
 		<br>
 	
 		<div style="background: #eee; width: 772px">
-			<div style="background: #ccc; padding: 10px; font-weight: bold">Donation</div>
+			<div style="background: #ccc; padding: 10px; font-weight: bold"><?php _e( 'Donation', 'woocommerce-google-adwords-conversion-tracking-tag' ) ?></div>
 		
 		    <table class="form-table" style="margin: 10px">
 		   	<tr>
 		   		<th scope="row">
-					<div style="padding: 10px">This plugin was developed by <a href="http://www.wolfundbaer.ch" target="_blank">Wolf & Bär</a><p>Buy me a beer if you like the plugin.<br>
-					If you want me to continue developing the plugin buy me a few more beers. Although, I probably will continue to develop the plugin anyway. It would be just much more fun if I had a few beers to celebrate my milestones.</div>
+					<div style="padding: 10px"><?php _e( 'This plugin was developed by', 'woocommerce-google-adwords-conversion-tracking-tag' ) ?> <a href="http://www.wolfundbaer.ch" target="_blank">Wolf & Bär GmbH</a><p><?php _e( 'Buy me a beer if you like the plugin.', 'woocommerce-google-adwords-conversion-tracking-tag' ) ?><br>
+					<?php _e( 'If you want me to continue developing the plugin buy me a few more beers. Although, I probably will continue to develop the plugin anyway. It would be just much more fun if I had a few beers to celebrate my milestones.', 'woocommerce-google-adwords-conversion-tracking-tag' ) ?></div>
 
 					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 					<input type="hidden" name="cmd" value="_s-xclick">
@@ -118,13 +148,13 @@ class WGACT{
 	//register_setting( 'plugin_options', 'plugin_options', 'wgact_plugin_options_validate' );
 		register_setting( 'wgact_plugin_options', 'wgact_plugin_options_1');
 		register_setting( 'wgact_plugin_options', 'wgact_plugin_options_2');
-		add_settings_section('wgact_plugin_main', 'WGACT Main Settings', array($this,'wgact_plugin_section_text'), 'do_wgact');
-		add_settings_field('wgact_plugin_text_string_1', 'Conversion ID', array($this,'wgact_plugin_setting_string_1'), 'do_wgact', 'wgact_plugin_main');
-		add_settings_field('wgact_plugin_text_string_2', 'Conversion label', array($this,'wgact_plugin_setting_string_2'), 'do_wgact', 'wgact_plugin_main');	
+		add_settings_section('wgact_plugin_main', esc_html__( 'Main Settings', 'woocommerce-google-adwords-conversion-tracking-tag' ), array($this,'wgact_plugin_section_text'), 'do_wgact');
+		add_settings_field('wgact_plugin_text_string_1', esc_html__( 'Conversion ID', 'woocommerce-google-adwords-conversion-tracking-tag' ), array($this,'wgact_plugin_setting_string_1'), 'do_wgact', 'wgact_plugin_main');
+		add_settings_field('wgact_plugin_text_string_2', esc_html__( 'Conversion Label', 'woocommerce-google-adwords-conversion-tracking-tag' ), array($this,'wgact_plugin_setting_string_2'), 'do_wgact', 'wgact_plugin_main');	
 	}
 
 	function wgact_plugin_section_text() {
-		echo '<p>Woocommerce Google AdWords conversion tracking tag</p>';
+		//echo '<p>Woocommerce Google AdWords conversion tracking tag</p>';
 	}
 
 	/*
@@ -179,57 +209,26 @@ class WGACT{
 	}
 	
 	// insert the Google AdWords tag into the page
-	public function GoogleAdWordsTag() {
+	public function GoogleAdWordsTag($order_id) {
 
-		global $woocommerce;
-
-		if(is_order_received_page()){
-
-			$conversion_id    = $this->get_conversion_id();
-			$conversion_label = $this->get_conversion_label();
-			$mc_prefix        = $this->get_mc_prefix();
-			
-/*
-You might ask yourself why I am not inserting the AdWords tracking code into the thankyou template of WooCommerce.
-A bit more information on that: Unfortunately there is a filter in WordPress (up to the current version 3.8.1) which messes up the Google AdWords tracking tag after injecting it into the thankyou page. The only workaround I've have found so far is to place the tracking code into the footer where the problematic WordPress filter is not being applied. This bug was reported years ago and is still an open issue: http://core.trac.wordpress.org/ticket/3670
-*/
-	
-			global $wpdb;
-
-			// get order from URL and evaluate order total
-			$order       = new WC_Order(wc_get_order_id_by_order_key($_GET['key']));
-			$order_total = $order->get_total();
-	
+		$conversion_id    = $this->get_conversion_id();
+		$conversion_label = $this->get_conversion_label();
+		$mc_prefix        = $this->get_mc_prefix();
+		
+		// get order from URL and evaluate order total
+		$order = new WC_Order( $order_id );
+		$order_total = $order->get_total();
 	?>
 	
 	<!-- Google Code for Sales (AdWords) Conversion Page -->
-	<script type="text/javascript">
-	/* <![CDATA[ */
-	var google_conversion_id = <?php echo $conversion_id; ?>;
-	var google_conversion_language = "en";
-	var google_conversion_format = "2";
-	var google_conversion_color = "ffffff";
-	var google_conversion_label = "<?php echo $conversion_label; ?>";
-	<?php 
 
-	if ( $order_total ) {
-		echo "var google_conversion_value = " . $order_total;
-	}
-
-	?>
-
-	/* ]]> */
-	</script>
-	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-	</script>
-	<noscript>
 	<div style="display:inline;">
-	<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/<?php echo $conversion_id; ?>/?value=<?php echo $order_total; ?>&amp;label=<?php echo $conversion_label; ?>&amp;guid=ON&amp;script=0"/>
+	<img height="1" width="1" style="border-style:none;" alt="" src="http<?php if(is_ssl()){echo "s";}?>://www.googleadservices.com/pagead/conversion/<?php echo $conversion_id; ?>/?value=<?php echo $order_total; ?>&amp;currency_code=<?php echo $order->get_order_currency(); ?>&amp;label=<?php echo $conversion_label; ?>&amp;guid=ON&amp;script=0"/>
 	</div>
-	</noscript>
+
 	
 	<?php 
-		} 
+
 	}
 }
 
